6391e161153373efe24d66a2c180c44eb312b267
Sebastián Peña <sipa2259@gmail.com>
2026-02-12 13:36:36 -0400

    Agregar props al componente Saludo

--- Proyecto-React/primer-componente/src/App.jsx
+++ Proyecto-React/primer-componente/src/App.jsx
@@ -1,20 +1,23 @@
 // Mi primer componente en React
-function Saludo() {
+function Saludo({ nombre }) {
   return (
-    <h2 style={{ color: 'green' }}>¡Hola, bienvenido a React!</h2>
+    <h2 style={{ color: 'green' }}>
+      ¡Hola {nombre}, bienvenido a React!
+      </h2>
   );
 }
 
 // Componente principal de la aplicación
 function App() {
   return (
-    <div style ={{ padding: '20 px' }}>
+    <div style ={{ padding: '20px' }}>
       <h1>Mi primera app react</h1>
       <p>Hoy es 11 de febrero y estoy aprendiendo React</p>
 
-      <saludo />
-      <saludo />
-      <saludo />
+      <Saludo nombre="Sebastian" />
+      <Saludo nombre="Developer"/>
+      <Saludo nombre="React Learner" />
+      <Saludo nombre="Security + Frontend" />
 
       <p>Reutilicé el componente 3 veces</p>
     </div>
