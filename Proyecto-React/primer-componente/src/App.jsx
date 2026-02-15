493dcd7bd3e97cada08af736b6fa06b8336bd3b7
Sebastian Peña <sipa2259@gmail.com>
2026-02-15 18:32:02 -0300

    feat(day-2): controlar datos desde el padre usando objeto

--- Proyecto-React/primer-componente/src/App.jsx
+++ Proyecto-React/primer-componente/src/App.jsx
@@ -1,25 +1,27 @@
 // Mi primer componente en React
 function Saludo({ nombre }) {
   return (
-    <h2 style={{ color: 'green' }}>
-      ¡Hola {nombre}, bienvenido a React!
-      </h2>
+    <h2 style={{ color: "green" }}>¡Hola {nombre}, bienvenido a React!</h2>
   );
 }
 
 // Componente principal de la aplicación
 function App() {
+  const usuarioPrincipal = {
+    nombre: "Sebastian",
+    rol: "Admin",
+  };
+
   return (
-    <div style ={{ padding: '20px' }}>
+    <div style={{ padding: "20px" }}>
       <h1>Mi primera app react</h1>
-      <p>Hoy es 11 de febrero y estoy aprendiendo React</p>
+      <p>Hoy es 15 de febrero y estoy aprendiendo React</p>
 
-      <Saludo nombre="Sebastian" />
-      <Saludo nombre="Developer"/>
-      <Saludo nombre="React Learner" />
-      <Saludo nombre="Security + Frontend" />
+      <Saludo nombre={usuarioPrincipal.nombre} rol={usuarioPrincipal.rol} />
 
-      <p>Reutilicé el componente 3 veces</p>
+      <Saludo nombre="Developer" rol="User" />
+      <Saludo nombre="React Learner" rol="Student" />
+      <Saludo nombre="Security + Frontend" rol="Builder" />
     </div>
   );
 }
